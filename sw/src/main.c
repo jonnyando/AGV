ad01798751342786778f4c6884403c51f5241e49
Jon <jonnyando@gmail.com>
2019-09-03 21:50:04 +1000

    simplified init code. Simplified overall file

--- sw/src/main.c
+++ sw/src/main.c
@@ -5,8 +5,7 @@ void SystemClock_Config(void);
 
 int __io_putchar(int ch)
 {
-    uint8_t ch8 = ch;
-    transmit_byte_uart1(ch8);
+    transmit_byte_uart(USART1, (uint8_t)ch);
     // ITM_SendChar(ch);
     return ch;
 }
@@ -15,7 +14,6 @@ volatile uint32_t myTicks = 0;
 void SysTick_Handler(void)
 {
     myTicks++;
-    // HAL_IncTick();
 }
 
 void myDelay(uint32_t mS){
@@ -23,49 +21,56 @@ void myDelay(uint32_t mS){
     while(myTicks<mS);
 }
 
-drv8303 drv;
 
 int main(void)
 {   
     // SystemInit();
     SystemClock_Config();
 
-    UART1_Init();  printf("\nuart initialized\n");
-    SPI1_Init();   printf("spi initialized\n");
-    GPIO_Init();   printf("gpio initialized\n");
-    TIM1_Init();   printf("tim1 initialized\n");
-    ADC1_Init();    printf("ADC1 initialized\n");
+    GPIO_Init();                printf("gpio initialized\n");
+    UART_Init(USART1);          printf("uart1 initialized\n");
+    UART_pin_Remap(USART1, 1);
+    SPI_Init(SPI1);             printf("spi1 initialized\n");
+    TIM1_Init();                printf("tim1 initialized\n");
+    // ADC1_Init();    printf("ADC1 initialized\n");
     
     // printf("RCC->CR\t\t");          print_reg(RCC->CR,     32);
 
     pin_reset(GPIOA, DC_CAL);
     printf("DC_CAL set LOW (off)\n");
-    pin_set(GPIOA, EN_GATE);
-    printf("\nEnabled DRV8303\n");
 
-    uint8_t tc[3] = "abc";
-    // transmit_byte_uart1(*tc);
-    // transmit_byte_uart1('\n');
-    transmit_uart1(tc, 3);
 
     uint16_t tx;
     uint16_t rx;
 
+    drv8303_TypeDef drv;
+    drv8303_Init(&drv, SPI1);
+    drv_update(&drv);
+
+
     // sets OC_ADJ_SET to 24 (Vds = 1.043v)
     // Disable OCP
     // sets drv to 3-PWM mode
-    uint16_t set_reg = 0b01000000000 | 0b00000110000 | 0b00000001100;
-    drv_write(0x02, set_reg);
+    // uint16_t set_reg = 0b01000000000 | 0b00000110000 | 0b00000001000;
+    // uint16_t set_reg = (CR1_OC_ADJ_SET_24 | CR1_OCP_MODE_DISABLED | CR1_PWM_MODE_3);
+    uint16_t set_reg = (CR1_OC_ADJ_SET_7 | CR1_PWM_MODE_3);
+    pin_set(GPIOA, EN_GATE);
+    myDelay(100);
+    drv_write(&drv, DRV_CTRL_1, set_reg);
+    printf("\nEnabled DRV8303\n");
 
-    // rx = drv_read(0x00);
-    // printf("Status register 1\t");  print_reg(rx, 16);
     // myDelay(10);
-    // rx = drv_read(0x01);
-    // printf("Status register 2\t");  print_reg(rx, 16);
-    // myDelay(10);
-    rx = drv_read(0x02);
+    myDelay(100);
+    printf("transmitting to drv\n");
+    rx = drv_read(&drv, DRV_STATUS_1);
+    printf("Status register 1\t");   print_reg(rx, 16);
+    rx = drv_read(&drv, DRV_STATUS_2);
+    printf("Status register 2\t");   print_reg(rx, 16);
+    rx = drv_read(&drv, DRV_CTRL_1);
     printf("Control register 1\t");  print_reg(rx, 16);
-    // myDelay(10);
+    rx = drv_read(&drv, DRV_CTRL_2);
+    printf("Control register 2\t");  print_reg(rx, 16);
+    myDelay(10);
     // rx = drv_read(0x03);
     // printf("Control register 2\t");  print_reg(rx, 16);
     // myDelay(10);
@@ -84,83 +89,21 @@ int main(void)
     float duty_cycle_c;
     float angle_step = 30;
     
+    uint8_t tc[3] = "abc";
+    // transmit_byte_uart1(*tc);
+    // transmit_byte_uart1('\n');
+    transmit_uart(USART1, tc, 3);
+
+    drv_update(&drv);
     
+    // drv_write(&drv, DRV_CTRL_1, set_reg);
     while (1){
-        // pin_set(GPIOA, LED);
-        // myDelay(1000);
-        // pin_reset(GPIOA, LED);
         // myDelay(200);
         // printf("beep..\n");
         // fflush(stdout);
         
-        // pin_set(GPIOA, INH_A);
-        // pin_set(GPIOB, INL_A);
-        // myDelay(5);
-        // pin_reset(GPIOA, INH_A);
-        // pin_reset(GPIOB, INL_A);
-        // myDelay(5);
-        // myDelay(t_del);
-        // pin_reset(GPIOA, INH_A); // 1
-        // pin_reset(GPIOB, INL_A);
-        // pin_set(GPIOA, INH_B);
-        // pin_reset(GPIOB, INL_B);
-        // pin_reset(GPIOA, INH_C);
-        // pin_set(GPIOB, INL_C);
-        // printf("1\n");
-        // myDelay(t_del);
-        // pin_reset(GPIOA, INH_A); // 2
-        // pin_set(GPIOB, INL_A);
-        // pin_set(GPIOA, INH_B);
-        // pin_reset(GPIOB, INL_B);
-        // pin_reset(GPIOA, INH_C);
-        // pin_reset(GPIOB, INL_C);
-        // printf("2\n");
-        // myDelay(t_del);
-        // pin_reset(GPIOA, INH_A); // 3
-        // pin_set(GPIOB, INL_A);
-        // pin_reset(GPIOA, INH_B);
-        // pin_reset(GPIOB, INL_B);
-        // pin_set(GPIOA, INH_C);
-        // pin_reset(GPIOB, INL_C);
-        // printf("3\n");
-        // myDelay(t_del);
-        // pin_reset(GPIOA, INH_A); // 4
-        // pin_reset(GPIOB, INL_A);
-        // pin_reset(GPIOA, INH_B);
-        // pin_set(GPIOB, INL_B);
-        // pin_set(GPIOA, INH_C);
-        // pin_reset(GPIOB, INL_C);
-        // printf("4\n");
-        // myDelay(t_del);
-        // pin_set(GPIOA, INH_A); // 5
-        // pin_reset(GPIOB, INL_A);
-        // pin_reset(GPIOA, INH_B);
-        // pin_set(GPIOB, INL_B);
-        // pin_reset(GPIOA, INH_C);
-        // pin_reset(GPIOB, INL_C);
-        // printf("5\n");
-        // myDelay(t_del);
-        // pin_set(GPIOA, INH_A); // 6
-        // pin_reset(GPIOB, INL_A);
-        // pin_reset(GPIOA, INH_B);
-        // pin_reset(GPIOB, INL_B);
-        // pin_reset(GPIOA, INH_C);
-        // pin_set(GPIOB, INL_C);
-        // printf("6\n");
 
-        // myDelay(10);
         myDelay(10);
-        // rx = drv_read(0x00);
-        // printf("Status register 1\t");  print_reg(rx, 16);
-        // myDelay(1);
-        // rx = drv_read(0x01);
-        // printf("Status register 2\t");  print_reg(rx, 16);
-        // myDelay(1);
-        // rx = drv_read(0x02);
-        // printf("Control register 1\t");  print_reg(rx, 16);
-        // myDelay(1);
-        // rx = drv_read(0x03);
-        // printf("Control register 2\t");  print_reg(rx, 16);
 
         if(!pin_read(GPIOA, nOCTW)){
             printf("nOCTW is LOW\n");
@@ -228,18 +171,16 @@ void SystemClock_Config(void)
     SysTick_Config(64000);
     NVIC_SetPriority(SysTick_IRQn, 0);
 
-
-
 }
 
 static void GPIO_Init(void)
 {
     /* GPIO Ports Clock Enable */
-    RCC->APB2ENR |= (1 << 0); // enable AFIO
-    RCC->APB2ENR |= (1 << 2); // enable GPIOA
-    RCC->APB2ENR |= (1 << 3); // enable GPIOB
-    RCC->APB2ENR |= (1 << 4); // enable GPIOC
-    RCC->APB2ENR |= (1 << 5); // enable GPIOD
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN; // enable AFIO
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; // enable GPIOA
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN; // enable GPIOB
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // enable GPIOC
+    RCC->APB2ENR |= RCC_APB2ENR_IOPDEN; // enable GPIOD
 
     pin_mode(GPIOA, nOCTW,     GPIO_IN_PULL);
     pin_pullmode(GPIOA, nOCTW, GPIO_PULLUP);
@@ -256,4 +197,14 @@ static void GPIO_Init(void)
     pin_mode(GPIOB, INL_B,     GPIO_OUT_PP);
     pin_mode(GPIOB, INL_C,     GPIO_OUT_PP);
     pin_mode(GPIOA, LED,       GPIO_OUT_PP);
+
+    // SPI Pin setup
+    pin_mode(GPIOA, 4, GPIO_OUT_PP); // NSS
+    pin_mode(GPIOA, 5, GPIO_AF_PP);  // SCK,
+    pin_mode(GPIOA, 6, GPIO_IN_FL);  // MISO
+    pin_mode(GPIOA, 7, GPIO_AF_PP);  // MOSI
+
+    // setup GPIO pins for USART1
+    pin_mode(GPIOB, 6, GPIO_AF_PP); // TX PB6
+    pin_mode(GPIOB, 7, GPIO_IN_FL); // RX PB7
 }
